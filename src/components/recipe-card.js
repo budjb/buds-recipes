b6068dc5a27125a02654bdebe201520428ada1d9
Bud Byrd <byrd.bud@heb.com>
2021-06-19 17:03:30 -0500

    Add recipe card hover animation.

--- src/components/recipe-card.js
+++ src/components/recipe-card.js
@@ -6,13 +6,11 @@ import '../scss/recipe-card.scss';
 export const RecipeCard = ({ path, name, photo, preview }) => {
   return (
     <div className="col">
-      <Link to={`/${path}`} className="recipe-card">
-        <div className="card shadow border-0 h-100">
-          <GatsbyImage alt={name} image={photo} className="card-img-top" />
-          <div className="card-body">
-            <h5 className="card-title">{name}</h5>
-            <p className="card-text">{preview}</p>
-          </div>
+      <Link to={`/${path}`} className="recipe-card card shadow border-0 h-100">
+        <GatsbyImage alt={name} image={photo} className="card-img-top" />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">{preview}</p>
         </div>
       </Link>
     </div>
