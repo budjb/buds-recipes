8efe11a20dc5c3387b93eb4e59e3d42b8416e65e
Bud Byrd <byrd.bud@heb.com>
2021-07-07 17:22:16 -0500

    Separate the off-canvas component into its own file.

---
+++ src/components/off-canvas.js
@@ -0,0 +1,222 @@
+import React, { forwardRef, useCallback, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import _ from 'lodash';
+import { useSwipeable } from 'react-swipeable';
+
+const doTransition = (element, duration, percent) => {
+  element.style.transform = `translateX(-${percent}%)`;
+  element.style.transitionDuration = duration + 'ms';
+};
+
+export const OffCanvas = forwardRef(({ children, duration = 300, threshold = 33 }, ref) => {
+  /**
+   * Track whether the initial swipe was either to the left or right,
+   * and lock the axis (open/close off-canvas vs vertical scroll) based on
+   * the original scroll direction for the duration of the swipe.
+   */
+  const [isHorizontalSwipe, setIsHorizontalSwipe] = useState(false);
+
+  /**
+   * Whether the off-canvas is open or closed.
+   */
+  const [isOpen, setOpen] = useState(false);
+
+  /**
+   * Reference to the off-canvas element.
+   *
+   * @type {React.RefObject<Element>}
+   */
+  const offCanvasRef = useRef();
+
+  /**
+   * Opens the off-canvas with the configured duration.
+   *
+   * @param d
+   */
+  const open = useCallback(
+    (d = duration) => {
+      doTransition(offCanvasRef.current, d, 100);
+      setOpen(true);
+    },
+    [offCanvasRef, setOpen, duration]
+  );
+
+  /**
+   * Closes the off-canvas with the configured duration.
+   */
+  const close = useCallback(
+    (d = duration) => {
+      doTransition(offCanvasRef.current, d, 0);
+      setOpen(false);
+    },
+    [offCanvasRef, setOpen, duration]
+  );
+
+  /**
+   * Expose open and close methods to the provided ref.
+   */
+  useImperativeHandle(ref, () => ({
+    open: open,
+    close: close,
+  }));
+
+  /**
+   * Event handler for the very beginning of a swipe.
+   */
+  const onSwipeStart = eventData => {
+    if (['Left', 'Right'].includes(eventData.dir)) {
+      setIsHorizontalSwipe(true);
+    }
+  };
+
+  /**
+   * Event handler for when a swipe has completed.
+   */
+  const onSwiped = eventData => {
+    if (!isHorizontalSwipe) {
+      return;
+    }
+
+    const element = offCanvasRef.current;
+
+    const velocity = Math.abs(eventData.vxvy[0]);
+    const width = element.clientWidth;
+    const deltaX = _.clamp(isOpen ? eventData.deltaX : eventData.deltaX * -1, 0, width);
+    const deltaPercent = (deltaX / width) * 100;
+    const animationTime = Math.min(duration, (width - deltaX) / velocity);
+
+    if (isOpen) {
+      if (deltaPercent < threshold) {
+        doTransition(element, duration, 100);
+      } else {
+        doTransition(element, animationTime, 0);
+        setOpen(false);
+      }
+    } else {
+      if (deltaPercent < threshold) {
+        doTransition(element, duration, 0);
+      } else {
+        doTransition(element, animationTime, 100);
+        setOpen(true);
+      }
+    }
+
+    setIsHorizontalSwipe(false);
+  };
+
+  /**
+   * Event handler for when a swipe is ongoing.
+   *
+   * @param eventData
+   */
+  const onSwiping = eventData => {
+    if (!isHorizontalSwipe) {
+      return;
+    }
+
+    const element = offCanvasRef.current;
+
+    const width = element.clientWidth;
+    const deltaX = _.clamp(isOpen ? eventData.deltaX : eventData.deltaX * -1, 0, width);
+    const deltaPercent = (deltaX / width) * 100;
+    const percent = isOpen ? 100 - deltaPercent : deltaPercent;
+
+    doTransition(element, 0, percent);
+  };
+
+  /**
+   * Swipe configuration.
+   *
+   * @type {SwipeableHandlers}
+   */
+  const swipeHandlers = useSwipeable({
+    delta: 16,
+    onSwiped,
+    onSwiping,
+    onSwipeStart,
+  });
+
+  /**
+   * Register the swipe event handlers with the document.
+   */
+  useEffect(() => {
+    swipeHandlers.ref(document.documentElement);
+  }, [swipeHandlers]);
+
+  /**
+   * Prevent vertical scrolling when a horizontal swipe is ongoing.
+   */
+  useEffect(() => {
+    if (isHorizontalSwipe) {
+      if (document.scrollingElement) {
+        document.scrollingElement.classList.add('lock-scroll');
+      }
+    } else {
+      if (document.scrollingElement) {
+        document.scrollingElement.classList.remove('lock-scroll');
+      }
+    }
+  }, [isHorizontalSwipe]);
+
+  /**
+   * Lock the body when the off-canvas is open on mobile clients.
+   */
+  useEffect(() => {
+    if (isOpen) {
+      if (document.scrollingElement) {
+        document.scrollingElement.classList.add('lock-scroll-lg');
+      }
+    } else {
+      if (document.scrollingElement) {
+        document.scrollingElement.classList.remove('lock-scroll-lg');
+      }
+    }
+  }, [offCanvasRef, isOpen, duration]);
+
+  /**
+   * Hand click events so that any click outside the off-canvas when it is shown will close it.
+   *
+   * @type {function(*): void}
+   */
+  const handleMenuClick = useCallback(
+    event => {
+      if (isOpen && offCanvasRef.current && !offCanvasRef.current.contains(event.target)) {
+        close();
+      }
+    },
+    [offCanvasRef, isOpen, close]
+  );
+
+  /**
+   * Handle escape key to close menu when it is open.
+   *
+   * @type {function(*): void}
+   */
+  const handleEscape = useCallback(
+    event => {
+      if (event.keyCode === 27 && isOpen) {
+        event.preventDefault();
+        close();
+      }
+    },
+    [isOpen, close]
+  );
+
+  /**
+   * Register click and keyboard handlers.
+   */
+  useEffect(() => {
+    document.addEventListener('mousedown', handleMenuClick);
+    document.addEventListener('keydown', handleEscape);
+
+    return () => {
+      document.removeEventListener('mousedown', handleMenuClick);
+      document.removeEventListener('keydown', handleEscape);
+    };
+  });
+
+  return (
+    <div className="offcanvas-collapse" ref={offCanvasRef}>
+      {children}
+    </div>
+  );
+});
--- src/templates/layout.js
+++ src/templates/layout.js
@@ -1,231 +1,12 @@
-import React, { forwardRef, useCallback, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { graphql, Link, navigate, StaticQuery } from 'gatsby';
 import { formatCategorySlug } from '../util';
+import { OffCanvas } from '../components/off-canvas';
 import _ from 'lodash';
 
 import '../scss/layout.scss';
 import { Helmet } from 'react-helmet';
 import { GatsbyImage } from 'gatsby-plugin-image';
-import { useSwipeable } from 'react-swipeable';
-
-const doTransition = (element, duration, percent) => {
-  element.style.transform = `translateX(-${percent}%)`;
-  element.style.transitionDuration = duration + 'ms';
-};
-
-const SideBar = forwardRef(({ children, duration = 300, threshold = 33 }, ref) => {
-  /**
-   * Track whether the initial swipe was either to the left or right,
-   * and lock the axis (open/close sidebar vs vertical scroll) based on
-   * the original scroll direction for the duration of the swipe.
-   */
-  const [isHorizontalSwipe, setIsHorizontalSwipe] = useState(false);
-
-  /**
-   * Whether the sidebar is open or closed.
-   */
-  const [isOpen, setOpen] = useState(false);
-
-  /**
-   * Reference to the sidebar element.
-   *
-   * @type {React.RefObject<Element>}
-   */
-  const offCanvasRef = useRef();
-
-  /**
-   * Opens the sidebar with the configured duration.
-   *
-   * @param d
-   */
-  const open = useCallback(
-    (d = duration) => {
-      doTransition(offCanvasRef.current, d, 100);
-      setOpen(true);
-    },
-    [offCanvasRef, setOpen, duration]
-  );
-
-  /**
-   * Closes the sidebar with the configured duration.
-   */
-  const close = useCallback(
-    (d = duration) => {
-      doTransition(offCanvasRef.current, d, 0);
-      setOpen(false);
-    },
-    [offCanvasRef, setOpen, duration]
-  );
-
-  /**
-   * Expose open and close methods to the provided ref.
-   */
-  useImperativeHandle(ref, () => ({
-    open: open,
-    close: close,
-  }));
-
-  /**
-   * Event handler for the very beginning of a swipe.
-   */
-  const onSwipeStart = eventData => {
-    if (['Left', 'Right'].includes(eventData.dir)) {
-      setIsHorizontalSwipe(true);
-    }
-  };
-
-  /**
-   * Event handler for when a swipe has completed.
-   */
-  const onSwiped = eventData => {
-    if (!isHorizontalSwipe) {
-      return;
-    }
-
-    const element = offCanvasRef.current;
-
-    const velocity = Math.abs(eventData.vxvy[0]);
-    const width = element.clientWidth;
-    const deltaX = _.clamp(isOpen ? eventData.deltaX : eventData.deltaX * -1, 0, width);
-    const deltaPercent = (deltaX / width) * 100;
-    const animationTime = Math.min(duration, (width - deltaX) / velocity);
-
-    if (isOpen) {
-      if (deltaPercent < threshold) {
-        doTransition(element, duration, 100);
-      } else {
-        doTransition(element, animationTime, 0);
-        setOpen(false);
-      }
-    } else {
-      if (deltaPercent < threshold) {
-        doTransition(element, duration, 0);
-      } else {
-        doTransition(element, animationTime, 100);
-        setOpen(true);
-      }
-    }
-
-    setIsHorizontalSwipe(false);
-  };
-
-  /**
-   * Event handler for when a swipe is ongoing.
-   *
-   * @param eventData
-   */
-  const onSwiping = eventData => {
-    if (!isHorizontalSwipe) {
-      return;
-    }
-
-    const element = offCanvasRef.current;
-
-    const width = element.clientWidth;
-    const deltaX = _.clamp(isOpen ? eventData.deltaX : eventData.deltaX * -1, 0, width);
-    const deltaPercent = (deltaX / width) * 100;
-    const percent = isOpen ? 100 - deltaPercent : deltaPercent;
-
-    doTransition(element, 0, percent);
-  };
-
-  /**
-   * Swipe configuration.
-   *
-   * @type {SwipeableHandlers}
-   */
-  const swipeHandlers = useSwipeable({
-    delta: 16,
-    onSwiped,
-    onSwiping,
-    onSwipeStart,
-  });
-
-  /**
-   * Register the swipe event handlers with the document.
-   */
-  useEffect(() => {
-    swipeHandlers.ref(document.documentElement);
-  }, [swipeHandlers]);
-
-  /**
-   * Prevent vertical scrolling when a horizontal swipe is ongoing.
-   */
-  useEffect(() => {
-    if (isHorizontalSwipe) {
-      if (document.scrollingElement) {
-        document.scrollingElement.classList.add('lock-scroll');
-      }
-    } else {
-      if (document.scrollingElement) {
-        document.scrollingElement.classList.remove('lock-scroll');
-      }
-    }
-  }, [isHorizontalSwipe]);
-
-  /**
-   * Lock the body when the sidebar is open on mobile clients.
-   */
-  useEffect(() => {
-    if (isOpen) {
-      if (document.scrollingElement) {
-        document.scrollingElement.classList.add('lock-scroll-lg');
-      }
-    } else {
-      if (document.scrollingElement) {
-        document.scrollingElement.classList.remove('lock-scroll-lg');
-      }
-    }
-  }, [offCanvasRef, isOpen, duration]);
-
-  /**
-   * Hand click events so that any click outside the sidebar when it is shown will close it.
-   *
-   * @type {function(*): void}
-   */
-  const handleMenuClick = useCallback(
-    event => {
-      if (isOpen && offCanvasRef.current && !offCanvasRef.current.contains(event.target)) {
-        close();
-      }
-    },
-    [offCanvasRef, isOpen, close]
-  );
-
-  /**
-   * Handle escape key to close menu when it is open.
-   *
-   * @type {function(*): void}
-   */
-  const handleEscape = useCallback(
-    event => {
-      if (event.keyCode === 27 && isOpen) {
-        event.preventDefault();
-        close();
-      }
-    },
-    [isOpen, close]
-  );
-
-  /**
-   * Register click and keyboard handlers.
-   */
-  useEffect(() => {
-    document.addEventListener('mousedown', handleMenuClick);
-    document.addEventListener('keydown', handleEscape);
-
-    return () => {
-      document.removeEventListener('mousedown', handleMenuClick);
-      document.removeEventListener('keydown', handleEscape);
-    };
-  });
-
-  return (
-    <div className="offcanvas-collapse" ref={offCanvasRef}>
-      {children}
-    </div>
-  );
-});
 
 const topCategories = categories => {
   return _.orderBy(categories, ['totalCount', 'fieldValue'], ['desc', 'asc']).splice(0, 5);
@@ -309,7 +90,7 @@ const Layout = ({ children, className, title, query = '' }) => {
               </button>
             </div>
 
-            <SideBar ref={sidebarRef}>
+            <OffCanvas ref={sidebarRef}>
               <div className="d-flex justify-content-end my-3">
                 <button
                   type="button"
@@ -339,7 +120,7 @@ const Layout = ({ children, className, title, query = '' }) => {
                   <small>More...</small>
                 </Link>
               </nav>
-            </SideBar>
+            </OffCanvas>
           </header>
 
           <main className={contentProps.join(' ')}>{children}</main>
